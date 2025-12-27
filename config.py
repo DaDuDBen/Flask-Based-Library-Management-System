870e852ce0ef8cc39c5f263022d1987c1eefefcf
Ezhil nilavan B <111113928+DaDuDBen@users.noreply.github.com>
2025-12-27 21:36:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 class Config:
     SECRET_KEY = "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///library.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/library.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FINE_PER_DAY = 10
+
